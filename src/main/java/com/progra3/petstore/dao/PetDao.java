23d0259f0152a0304da80c24d54e2eb0076605bb
Célita Morales <cmoralesm37@miumg.edu.gt>
2022-06-10 04:26:40 -0600

    swagger

--- src/main/java/com/progra3/petstore/dao/PetDao.java
+++ src/main/java/com/progra3/petstore/dao/PetDao.java
@@ -1,7 +1,8 @@
 package com.progra3.petstore.dao;
 
 
+import com.progra3.petstore.entities.Pet;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetDao extends CrudRepository{
+public interface PetDao extends CrudRepository<Pet, Long> {
 }
